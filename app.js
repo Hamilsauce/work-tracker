00d132acd158c652c4f7db2108b7741c18ac2dbf
hamilsauce <jake@seahag.onmicrosoft.com>
2021-04-22 20:26:38 -0700

    hk

---
+++ app.js
@@ -0,0 +1,7 @@
+var study = new Vue({
+  el: "#vue-app",
+  data: {
+    heading: "Vue.js",
+    text: "Give a man a fish and you feed him for a day. Teach a man to fish, and you can sell him some tackle."
+  }
+});
