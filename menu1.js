4f27ed0946745834d7dd132a9e00e33980fff3f0
Jacob Hamilton <jake@seahag.onmicrosoft.com>
2021-04-22 22:37:48 -0500

    Add files via upload

---
+++ menu1.js
@@ -0,0 +1,105 @@
+const toggleNav = () => {
+	const nav = document.querySelector('.nav1');
+	// nav.style.width = '250px';
+	if (nav.classList.contains('navExpand')) {
+		setTimeout(() => {
+			nav.classList.toggle('navExpand')
+		}, 200)
+	} else {
+		setTimeout(() => {
+			nav.classList.toggle('navExpand')
+		}, 200)
+	}
+}
+const closeNav = () => {
+	const nav = document.querySelector('.nav1');
+	// nav.style.width = '0px';
+	nav.classList.toggle('navExpand')
+
+}
+
+const handleCardData = (action, key, cardData) => {
+	const store = window.localStorage;
+	if (action == 'get') {
+		let fetchedData = store.getItem(key)
+		return fetchedData
+	} else if (action == 'set') {
+		store.setItem(key, cardData)
+	}
+}
+
+document.querySelector('.menu-button')
+	.addEventListener('click', e => {
+		toggleNav();
+	})
+
+document.querySelector('.close-nav')
+	.addEventListener('click', e => {
+		toggleNav();
+		window.navigator.vibrate(1000); // 
+	})
+
+// window.navigator.vibrate(1000); // 
+
+document.querySelector('.form-modal-container')
+	.addEventListener('click', e => {
+		e.preventDefault()
+		e.stopPropagation();
+	})
+
+document.querySelector('.userform-cancel-button')
+	.addEventListener('click', e => {
+		e.preventDefault()
+		e.stopPropagation();
+		let modal = document.querySelector('.form-modal-container')
+		modal.classList.add('hide')
+	})
+
+document.querySelector('.userform-submit-button')
+	.addEventListener('click', e => {
+		e.preventDefault()
+		e.stopPropagation();
+
+		let name = document.querySelector('.author-name-input').value
+		let date = document.querySelector('.date-input').value
+		let header = document.querySelector('.header-input').value
+		let content = document.querySelector('.content-input').value
+		let app = document.querySelector('.app')
+
+		let appMarkup = app.innerHTML
+		let newCard = `
+			<div class="card">
+					<div class="card-date">${header}</div>
+					<div class="card-hours">
+						<div class="card-date-container">${date}</div>
+						<div class="card-author-container">${name}</div>
+					</div>
+					<div class="card-details">${content}</div>
+				</div>
+				</div>
+		`;
+		let newAppHtml = newCard.concat(appMarkup)
+		app.innerHTML = newAppHtml
+		handleCardData('set', 'cardData', newAppHtml)
+
+		let modal = document.querySelector('.form-modal-container')
+		modal.classList.add('hide')
+	})
+
+document.querySelector('.new-card-link')
+	.addEventListener('click', e => {
+		let nav = document.querySelector('.nav1')
+		// nav.classList.add('hide')
+		toggleNav()
+
+		let modal = document.querySelector('.form-modal-container')
+		modal.classList.remove('hide')
+	})
+
+window.onload = e => {
+	let app = document.querySelector('.app')
+	let appMarkup = handleCardData('get', 'cardData')
+	app.innerHTML = appMarkup
+
+	console.log('page is fully loaded');
+};
