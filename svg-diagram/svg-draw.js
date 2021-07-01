28e6b66c10cc3c8cc398a2640851993991daf44e
hamilsauce <jake@seahag.onmicrosoft.com>
2021-06-30 22:15:15 -0700

    svg draw touch

--- svg-diagram/svg-draw.js
+++ svg-diagram/svg-draw.js
@@ -31,10 +31,67 @@ const animate = () => {
 }
 
 
-// !!!!MOUSE PAINTING
-
 let clicks = 0;
 let radius = 2;
+
+// !!! TOUCH PAINTING
+
+
+function paintWithFinger(e) {
+	const targ = e.target
+	if (!targ.closest('#svg')) return;
+	const eraseMode = e.ctrlKey;
+	const touches = e.touches
+
+	if ((targ.id === 'svg' || targ.classList.contains('circle')) && !eraseMode) {
+		if (targ.children.length > 250) {
+			const firstCircleChild = [...targ.children].find(child => child.classList.contains('circle'));
+			targ.removeChild(firstCircleChild);
+		}
+
+		let newStroke = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+		let xPos = Math.round(parseInt(e.touches[0].clientX));
+		let yPos = e.touches[0].clientY;
+
+		newStroke.setAttribute("r", radius);
+		newStroke.setAttribute("cx", xPos);
+		newStroke.setAttribute("cy", yPos);
+		newStroke.setAttribute("fill", "#000");
+		newStroke.classList.add('circle', `circle${targ.children.length + 1}`)
+		targ.appendChild(newStroke)
+
+	} else if (targ.classList.contains('circle') && eraseMode) {
+		svgEl = targ.parentElement;
+		svg.removeChild(targ)
+	}
+}
+
+const startTouch = (e) => {
+	const { target, touches } = e
+	if (!target.closest('#svg') && !target.classList.contains('circle')) return;
+
+	if (touches && touches.length === 1 && (target.id === 'svg' || target.classList.contains('circle'))) {
+		paintWithMouse(e)
+		if (clicks <= 5) {
+			++clicks
+			radius += clicks;
+		} else {
+			clicks = 0;
+			radius = 2;
+		}
+		document.addEventListener('touchmove', paintWithFinger, true);
+
+		document.addEventListener('touchend', function(e) {
+			document.removeEventListener("touchmove", paintWithMouse, true);
+		});
+	}
+}
+
+document.addEventListener('touchstart', startTouch)
+
+
+// !!! MOUSE PAINTING
+
 
 function paintWithMouse(e) {
 	const targ = e.target
@@ -45,7 +102,7 @@ function paintWithMouse(e) {
 		if (targ.children.length > 250) {
 			const firstCircleChild = [...targ.children].find(child => child.classList.contains('circle'));
 			targ.removeChild(firstCircleChild);
-    }
+		}
 
 		let newStroke = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 		newStroke.setAttribute("r", radius);
@@ -53,7 +110,7 @@ function paintWithMouse(e) {
 		newStroke.setAttribute("cy", e.y);
 		newStroke.setAttribute("fill", "#000");
 		newStroke.classList.add('circle', `circle${targ.children.length + 1}`)
-    targ.appendChild(newStroke)
+		targ.appendChild(newStroke)
 
 	} else if (targ.classList.contains('circle') && eraseMode) {
 		svgEl = targ.parentElement;
@@ -61,8 +118,8 @@ function paintWithMouse(e) {
 	}
 }
 
-document.addEventListener('mousedown', function (e) {
-  e.stopPropagation()
+document.addEventListener('mousedown', function(e) {
+	e.stopPropagation()
 	const targ = e.target
 	if (!targ.closest('#svg') && !targ.classList.contains('circle')) return;
 	paintWithMouse(e)
@@ -76,9 +133,9 @@ document.addEventListener('mousedown', function (e) {
 		}
 	}
 
-  document.addEventListener('mousemove', paintWithMouse, true);
+	document.addEventListener('mousemove', paintWithMouse, true);
 
-	document.addEventListener('mouseup', function (e) {
+	document.addEventListener('mouseup', function(e) {
 		document.removeEventListener("mousemove", paintWithMouse, true);
 	});
 })
